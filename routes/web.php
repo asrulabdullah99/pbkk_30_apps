1cb3b6428f3909b2bcb80a78f3c8bcaca2f1c956
asrulabdullah99 <srulabdullah@gmail.com>
2024-03-17 10:31:53 +0700

    pekerjaan yang terbaru

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/greeting', function () {
+    return 'Selamat datang di Laravel 11';
+});
+
+
+Route::get('/welcome', function () {
+    return 'Selamat datang di Laravel 11';
+});
