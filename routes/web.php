f84451b366f29f555a68a88e6d9cb12f86847b78
asrulabdullah99 <srulabdullah@gmail.com>
2024-03-18 11:55:05 +0700

    terbaru

--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/greeting', function () {
-    return 'Selamat datang di Laravel 11';
-});
 
-
-Route::get('/welcome', function () {
-    return 'Selamat datang di Laravel 11';
-});
